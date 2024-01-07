81432c079ef1594ccda0019ae1e34c39a5dd119a
Brian K Fung <64922294+ufbfung@users.noreply.github.com>
2024-01-06 23:35:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1SDk3n-hNh-G1jBHXWHX5UoxkHfJb_KcV
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import json
